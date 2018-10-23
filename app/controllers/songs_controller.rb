5d21ace9532bf878288a023072e9bf0873a051e5
Juan Tenezaca <tenj34@gmail.com>
2018-10-23 21:20:08 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,14 @@
 require 'pry'
 require 'rack-flash'
  class SongsController < ApplicationController
-  
+
   use Rack::Flash
-  
+
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
   end
-  
+
   get '/songs/new' do
     erb :'/songs/new'
   end
