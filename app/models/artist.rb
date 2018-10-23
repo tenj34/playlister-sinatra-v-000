407176560b0915e25bc8ce972fa61ef6bb9fc026
Juan Tenezaca <tenj34@gmail.com>
2018-10-23 21:22:24 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,8 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
-  
-  def slug 
+
+  def slug
     name.downcase.gsub(" ","-")
   end
    def self.find_by_slug(slug)
